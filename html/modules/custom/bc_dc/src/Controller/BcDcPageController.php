31ba034f4610f85a3862ec71be57fc2da5a34e06
Liam Morland <liam@openplus.ca>
2023-05-08 17:16:05 -0400

    Add documentation to BcDcPageController

--- html/modules/custom/bc_dc/src/Controller/BcDcPageController.php
+++ html/modules/custom/bc_dc/src/Controller/BcDcPageController.php
@@ -13,6 +13,9 @@ class BcDcPageController extends ControllerBase {
   /**
    * Page callback to display a build page for a dataset.
    *
+   * This displays the data_set_build_page layout builder page, allowing it to
+   * be placed at the URL path configured in routing.
+   *
    * @param Drupal\node\NodeInterface $node
    *   The node for which to display the build page.
    *
