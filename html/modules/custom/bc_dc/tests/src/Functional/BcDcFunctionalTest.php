96a946d06d5011af83d65610dc6a71a96581da1d
Liam Morland <liam@openplus.ca>
2023-05-09 16:09:07 -0400

    Add basic test of build page

--- html/modules/custom/bc_dc/tests/src/Functional/BcDcFunctionalTest.php
+++ html/modules/custom/bc_dc/tests/src/Functional/BcDcFunctionalTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\bc_dc\Functional;
 
+use Drupal\Core\Config\FileStorage;
 use Drupal\Tests\BrowserTestBase;
 
 /**
@@ -29,6 +30,32 @@ class BcDcFunctionalTest extends BrowserTestBase {
   public function test(): void {
     $this->drupalGet('');
     $this->assertSession()->statusCodeEquals(200);
+
+    // Login as admin.
+    $this->drupalLogin($this->rootUser);
+
+    // Import config, like `drush config:import`.
+    $config_path = DRUPAL_ROOT . '/../config/sync';
+    $config_source = new FileStorage($config_path);
+    \Drupal::service('config.installer')->installOptionalConfig($config_source);
+
+    // Create a data_set node.
+    $this->drupalGet('node/add/data_set', ['query' => ['display' => 'data_set_description']]);
+    $this->assertSession()->statusCodeEquals(200);
+    $edit = [
+      'edit-title-0-value' => 'Data set ' . $this->randomString(),
+    ];
+    $this->submitForm($edit, 'Save');
+    $this->assertSession()->pageTextContains('Data set ' . $edit['edit-title-0-value'] . ' has been created');
+
+    // Admin has access to build page.
+    $this->drupalGet('node/1/build');
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Anonymous has no access to build page.
+    $this->drupalLogout();
+    $this->drupalGet('node/1/build');
+    $this->assertSession()->statusCodeEquals(403);
   }
 
 }
