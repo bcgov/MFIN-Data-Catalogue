29387a382b0586fb48b9c680e42e9715abf490f6
sylus <sylus1984@gmail.com>
2023-05-14 12:11:02 -0400

    feat(ci): Update composer.lock

--- scripts/ScriptHandler.php
+++ scripts/ScriptHandler.php
@@ -5,6 +5,7 @@ namespace BCGovC\MfinDataCatalogue;
 use Composer\Script\Event;
 use Composer\Semver\Comparator;
 use Drupal\Core\Site\Settings;
+use Drupal\Core\Site\SettingsEditor;
 use DrupalFinder\DrupalFinder;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Path;
@@ -57,7 +58,7 @@ class ScriptHandler {
         'value' => Path::makeRelative($drupalFinder->getComposerRoot() . '/config/sync', $drupalRoot),
         'required' => TRUE,
       ];
-      drupal_rewrite_settings($settings, $drupalRoot . '/sites/default/settings.php');
+      SettingsEditor::rewrite($drupalRoot . '/sites/default/settings.php', $settings);
       $fs->chmod($drupalRoot . '/sites/default/settings.php', 0666);
       $event->getIO()->write("Created a sites/default/settings.php file with chmod 0666");
     }
