085590a485c7f289cef7aa7371e0c0f1e30ab3ab
Liam Morland <liam@openplus.ca>
2023-05-15 17:24:20 -0400

    Issue #2: Create table of data_set nodes on bc_dc.data_set_dashboard

--- html/modules/custom/bc_dc/src/Controller/BcDcPageController.php
+++ html/modules/custom/bc_dc/src/Controller/BcDcPageController.php
@@ -48,6 +48,64 @@ class BcDcPageController extends ControllerBase {
       ],
     ];
 
+    // Table of data_set nodes.
+    //
+    // Query for data_set nodes by this user that are unpublished.
+    $query = $this->entityTypeManager()->getStorage('node')->getQuery();
+    $query->accessCheck(FALSE);
+    $query->condition('type', 'data_set');
+    $query->condition('uid', $this->currentUser()->id());
+    $query->condition('status', NodeInterface::NOT_PUBLISHED);
+    $data_set_nids = $query->execute();
+    // Table headers.
+    $header = [
+      $this->t('Title'),
+      $this->t('Actions'),
+    ];
+    // Table rows. One row per data_set.
+    $rows = [];
+    foreach ($data_set_nids as $nid) {
+      $data_set = $this->entityTypeManager()->getStorage('node')->load($nid);
+
+      $rows[] = [
+        $data_set->getTitle(),
+        [
+          'data' => [
+            'view' => [
+              '#title' => $this->t('View'),
+              '#type' => 'link',
+              '#url' => $data_set->toUrl(),
+              '#attributes' => [
+                'class' => ['button'],
+                'aria-label' => $this->t('View @title', ['@title' => $data_set->getTitle()]),
+              ],
+            ],
+            'build' => [
+              '#title' => $this->t('Build'),
+              '#type' => 'link',
+              '#url' => Url::fromRoute('bc_dc.data_set_build_page', ['node' => $data_set->id()]),
+              '#attributes' => [
+                'class' => ['button'],
+                'aria-label' => $this->t('Build @title', ['@title' => $data_set->getTitle()]),
+              ],
+            ],
+          ],
+        ],
+      ];
+    }
+    // Theme the table.
+    $page['data_set-table'] = [
+      '#type' => 'table',
+      '#caption' => $this->t('My unpublished data sets'),
+      '#empty' => $this->t('No data sets to show.'),
+      '#header' => $header,
+      '#rows' => $rows,
+      '#sticky' => TRUE,
+      '#attributes' => [
+        'class' => ['data-set-table'],
+      ],
+    ];
+
     return $page;
   }
 
