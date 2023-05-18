bcgov/MFIN-Data-Catalogue

commit 764452e7fe0238efe166e4e237c49236a1aaa251

Issue #2: Show table of bookmarked data_set nodes on dashboard

html/modules/custom/bc_dc/src/Controller/BcDcPageController.php
@@ -81,6 +81,14 @@ class BcDcPageController extends ControllerBase {
     $data_set_nids = $query->execute();
     $page['data_set-table'] = $this->dataSetTableTheme($data_set_nids, 'data-set-table', $this->t('My unpublished data sets'));
 
+    // Table of data_set nodes bookmarked by this user.
+    $query = $this->entityTypeManager()->getStorage('flagging')->getQuery();
+    $query->accessCheck(FALSE);
+    $query->condition('flag_id', 'bookmark');
+    $query->condition('uid', $this->currentUser()->id());
+    $bookmark_nids = $query->execute();
+    $page['bookmark-table'] = $this->dataSetTableTheme($bookmark_nids, 'bookmark-table', $this->t('Bookmarked data sets'));
+
     return $page;
   }
 

commit 379ea707e2e1c73b121dce745d55faea56082f28

Issue #2: Correct which nodes appear as bookmarks on dashboard
Previously, it would use the IDs of the bookmark flags as the node IDs.

Fixes issue introduced in 764452e.

html/modules/custom/bc_dc/src/Controller/BcDcPageController.php
@@ -86,7 +86,15 @@ class BcDcPageController extends ControllerBase {
     $query->accessCheck(FALSE);
     $query->condition('flag_id', 'bookmark');
     $query->condition('uid', $this->currentUser()->id());
-    $bookmark_nids = $query->execute();
+    $bookmark_flagging_ids = $query->execute();
+    // Convert the array of IDs of the bookmark flags into an array of IDs of
+    // the nodes that are bookmarked.
+    $bookmarks = $this->entityTypeManager()->getStorage('flagging')->loadMultiple($bookmark_flagging_ids);
+    $bookmark_nids = [];
+    foreach ($bookmarks as $bookmark) {
+      $bookmark_nids[] = $bookmark->getFlaggableId();
+    }
+    // Generate the table.
     $page['bookmark-table'] = $this->dataSetTableTheme($bookmark_nids, 'bookmark-table', $this->t('Bookmarked data sets'));
 
     return $page;