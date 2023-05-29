eff5be912cf76626077aad38887ac2308bf8bf29
Liam Morland <liam@openplus.ca>
2023-05-29 14:33:39 -0400

    Issue #6: Create build page to replace layout builder build page

--- html/modules/custom/bc_dc/src/Controller/BcDcPageController.php
+++ html/modules/custom/bc_dc/src/Controller/BcDcPageController.php
@@ -34,24 +34,6 @@ class BcDcPageController extends ControllerBase {
   }
 
   /**
-   * Page callback to display a build page for a dataset.
-   *
-   * This displays the data_set_build_page layout builder page, allowing it to
-   * be placed at the URL path configured in routing.
-   *
-   * @param Drupal\node\NodeInterface $node
-   *   The node for which to display the build page.
-   *
-   * @return array
-   *   A render array for the build page.
-   */
-  public function buildPage(NodeInterface $node): array {
-    return $this->entityTypeManager()
-      ->getViewBuilder('node')
-      ->view($node, 'data_set_build_page');
-  }
-
-  /**
    * Page callback for the data set dashboard.
    *
    * @return array
@@ -147,7 +129,7 @@ class BcDcPageController extends ControllerBase {
             'build' => [
               '#title' => $this->t('Build'),
               '#type' => 'link',
-              '#url' => Url::fromRoute('bc_dc.data_set_build_page', ['node' => $data_set->id()]),
+              '#url' => Url::fromRoute('page_manager.page_view_data_set_build_data_set_build-block_display-0', ['node' => $data_set->id()]),
               '#attributes' => [
                 'class' => ['button'],
                 'aria-label' => $this->t('Build "@title".', ['@title' => $data_set->getTitle()]),
