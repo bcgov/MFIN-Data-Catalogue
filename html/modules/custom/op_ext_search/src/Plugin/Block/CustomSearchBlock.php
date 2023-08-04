bcgov/MFIN-Data-Catalogue

commit b570dde93bc6b820594ab5e6cc3252d55ca55baf

Issue #17: Add module op_ext_search from repo op_ext revision 2883141.

html/modules/custom/op_ext_search/src/Plugin/Block/CustomSearchBlock.php
@@ -1,0 +1,239 @@
+<?php
+
+namespace Drupal\op_ext_search\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\op_ext_search\Form\CustomSearchApiForm;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a custom search block.
+ *
+ * @package Drupal\custom_search_api_block\Plugin\Block
+ *
+ * @Block(
+ *  id = "custom_search_api_block",
+ *  admin_label = @Translation("Custom Search API Block"),
+ *  category = @Translation("Custom"),
+ * )
+ */
+class CustomSearchBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Constructs a CustomSearchBlock object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity_type.manager service.
+   * @param \Drupal\Core\Form\FormBuilderInterface $formBuilder
+   *   The form_builder service.
+   */
+  public function __construct(
+    array $configuration,
+    string $plugin_id,
+    mixed $plugin_definition,
+    protected EntityTypeManagerInterface $entityTypeManager,
+    protected FormBuilderInterface $formBuilder,
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): static {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('form_builder'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * This method sets the block default configuration.
+   */
+  public function defaultConfiguration(): array {
+    return [
+      'search' => [
+        'search_label' => 'Search',
+        'search_url' => '/search/site/',
+      ],
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * This method defines form elements for custom block configuration.
+   */
+  public function blockForm($form, FormStateInterface $form_state): array {
+    $form = parent::blockForm($form, $form_state);
+    $config = $this->getConfiguration();
+
+    $facetsList = [];
+
+    // Store Facets block list.
+    $facets = $this->entityTypeManager->getStorage('facets_facet')->getQuery()->execute();
+    foreach ($facets as $value) {
+      $facetsLoad = $this->entityTypeManager->getStorage('facets_facet')->load($value);
+
+      // Is taxonomy term.
+      if (isset($facetsLoad->getDataDefinition()->getSettings()['target_type']) && $facetsLoad->getDataDefinition()->getSettings()['target_type'] == 'taxonomy_term') {
+        $facetsList[$value] = $facetsLoad->getName();
+      }
+    }
+
+    $form['search'] = [
+      '#title' => $this->t('Search configuration'),
+      '#type' => 'details',
+    ];
+
+    $form['search']['label_sr_only'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Label shown on screen reader only'),
+      '#default_value' => $config['search']['label_sr_only'] ?? FALSE,
+    ];
+
+    $form['search']['search_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Label'),
+      '#maxlength' => 255,
+      '#default_value' => $config['search']['search_label'] ?? NULL,
+    ];
+
+    $form['search']['search_placeholder'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Placeholder'),
+      '#maxlength' => 255,
+      '#default_value' => $config['search']['search_placeholder'] ?? NULL,
+    ];
+
+    $form['search']['search_url'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('URL'),
+      '#maxlength' => 255,
+      '#default_value' => $config['search']['search_url'],
+      '#required' => TRUE,
+      '#element_validate' => [[static::class, 'validatePath']],
+    ];
+
+    $form['search']['search_btn_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Button label'),
+      '#maxlength' => 255,
+      '#default_value' => $config['search']['search_btn_label'] ?? NULL,
+    ];
+    $form['search']['search_input_size'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Set search input size'),
+      '#default_value' => $config['search']['search_input_size'] ?? NULL,
+    ];
+
+    $form['search']['search_input_size_value'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Enter the size value for search input field.'),
+      '#default_value' => $config['search']['search_input_size_value'] ?? NULL,
+      '#min' => 0,
+      '#max' => 999,
+      '#step' => 1,
+      '#states' => [
+        'visible' => [
+          ':input[name="settings[search][search_input_size]"]' => ['checked' => TRUE],
+        ],
+        'required' => [
+          ':input[name="settings[search][search_input_size]"]' => ['checked' => TRUE],
+        ],
+      ],
+    ];
+    $form['facets'] = [
+      '#title' => $this->t('Facet configuration'),
+      '#type' => 'details',
+    ];
+
+    foreach ($facetsList as $key => $value) {
+      $form['facets'][$key] = [
+        '#title' => $value,
+        '#type' => 'details',
+      ];
+
+      $form['facets'][$key]['enabled'] = [
+        '#type' => 'checkbox',
+        '#title' => $value,
+        '#default_value' => $config['facets'][$key]['enabled'] ?? FALSE,
+      ];
+
+      $form['facets'][$key]['override_title'] = [
+        '#type' => 'checkbox',
+        '#title' => $this->t('Override default title for @value', ['@value' => $value]),
+        '#default_value' => $config['facets'][$key]['override_title'] ?? FALSE,
+        '#states' => [
+          'visible' => [
+            ':input[name="settings[facets][' . $key . '][enabled]"]' => ['checked' => TRUE],
+          ],
+        ],
+      ];
+
+      $form['facets'][$key]['title'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('@value title', ['@value' => $value]),
+        '#default_value' => $config['facets'][$key]['title'] ?? '',
+        '#states' => [
+          'visible' => [
+            ':input[name="settings[facets][' . $key . '][override_title]"]' => ['checked' => TRUE],
+          ],
+          'required' => [
+            ':input[name="settings[facets][' . $key . '][override_title]"]' => ['checked' => TRUE],
+          ],
+        ],
+      ];
+    }
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state): void {
+    parent::blockSubmit($form, $form_state);
+    $this->configuration['search'] = $form_state->getValue('search');
+    $this->configuration['facets'] = $form_state->getValue('facets');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    $config = $this->getConfiguration();
+    return $this->formBuilder->getForm(CustomSearchApiForm::class, $config);
+  }
+
+  /**
+   * Callback for #element_validate for search_url.
+   */
+  public static function validatePath(array &$element, FormStateInterface $form_state, array &$complete_form): void {
+    // Ensure the path has a leading slash.
+    if ($value = trim($element['#value'], '/')) {
+      $value = '/' . $value;
+      $form_state->setValueForElement($element, $value);
+    }
+    // Check to make sure the path exists after stripping slashes.
+    else {
+      $form_state->setErrorByName('path', t('Path is required.'));
+    }
+  }
+
+}

html/modules/custom/op_ext_search/config/language/fr/page_manager.page_variant.site_search-panels_variant-0.yml
@@ -1,0 +1,3 @@
+variant_settings:
+  label: ''
+  page_title: Recherche

html/modules/custom/op_ext_search/config/language/fr/views.view.site_search.yml
@@ -1,0 +1,29 @@
+display:
+  default:
+    display_title: 'Par défaut'
+    display_options:
+      fields:
+        title:
+          separator: ', '
+        body:
+          separator: ', '
+      pager:
+        options:
+          tags:
+            next: 'Suivant ›'
+            previous: '‹ Précédent'
+            first: '« Premier'
+            last: 'Dernier »'
+          expose:
+            items_per_page_label: 'Éléments par page'
+            items_per_page_options_all_label: '- Tout -'
+            offset_label: Décalage
+      exposed_form:
+        options:
+          submit_button: Recherche
+          reset_button_label: Réinitialiser
+          exposed_sorts_label: 'Trier par'
+          sort_asc_label: Asc
+          sort_desc_label: Desc
+  block_1:
+    display_title: Bloc

html/modules/custom/op_ext_search/config/optional/facets.facet.content_type.yml
@@ -1,0 +1,64 @@
+langcode: en
+status: true
+dependencies:
+  config:
+    - search_api.index.default_index
+    - views.view.site_search
+  module:
+    - search_api
+id: content_type
+name: 'Content type'
+weight: 0
+min_count: 1
+url_alias: content_type
+facet_source_id: 'search_api:views_block__site_search__block_1'
+field_identifier: type
+query_operator: or
+hard_limit: 0
+exclude: false
+use_hierarchy: false
+keep_hierarchy_parents_active: false
+hierarchy:
+  type: taxonomy
+  config: {  }
+expand_hierarchy: false
+enable_parent_when_child_gets_disabled: true
+widget:
+  type: links
+  config:
+    show_numbers: true
+    soft_limit: 0
+    show_reset_link: false
+    reset_text: 'Show all'
+    hide_reset_when_no_selection: false
+    soft_limit_settings:
+      show_less_label: 'Show less'
+      show_more_label: 'Show more'
+empty_behavior:
+  behavior: none
+only_visible_when_facet_source_is_visible: false
+show_only_one_result: false
+show_title: false
+processor_configs:
+  display_value_widget_order:
+    processor_id: display_value_widget_order
+    weights:
+      sort: 40
+    settings:
+      sort: DESC
+  hierarchy_processor:
+    processor_id: hierarchy_processor
+    weights:
+      build: 100
+    settings: {  }
+  translate_entity:
+    processor_id: translate_entity
+    weights:
+      build: 5
+    settings: {  }
+  url_processor_handler:
+    processor_id: url_processor_handler
+    weights:
+      pre_query: 50
+      build: 15
+    settings: {  }

html/modules/custom/op_ext_search/config/optional/facets.facet_source.search_api__views_block__site_search__block_1.yml
@@ -1,0 +1,8 @@
+langcode: en
+status: true
+dependencies: {  }
+id: search_api__views_block__site_search__block_1
+name: 'search_api:views_block__site_search__block_1'
+filter_key: null
+url_processor: query_string
+breadcrumb: {  }

html/modules/custom/op_ext_search/config/optional/page_manager.page.site_search.yml
@@ -1,0 +1,11 @@
+langcode: en
+status: true
+dependencies: {  }
+id: site_search
+label: 'Site search'
+description: ''
+use_admin_theme: false
+path: /search/site
+access_logic: and
+access_conditions: {  }
+parameters: {  }

html/modules/custom/op_ext_search/config/optional/page_manager.page_variant.site_search-panels_variant-0.yml
@@ -1,0 +1,99 @@
+langcode: en
+status: true
+dependencies:
+  config:
+    - facets.facet.content_type
+    - page_manager.page.site_search
+    - views.view.site_search
+  module:
+    - facets
+    - panels
+    - views
+_core:
+  default_config_hash: L8yZJERwIYfavdnZ-MnTeZaRXg-8OD1pU6XSjq80rwY
+id: site_search-panels_variant-0
+label: 'Search site'
+weight: 0
+variant: panels_variant
+variant_settings:
+  id: panels_variant
+  label: null
+  weight: 0
+  uuid: 967380c9-4f82-4f33-9856-6ee7d8e6143d
+  blocks:
+    8c022726-d5ca-4c3a-a35d-c5dfa0457ebd:
+      id: 'views_block:site_search-block_1'
+      label: ''
+      label_display: '0'
+      provider: views
+      context_mapping: {  }
+      views_label: ''
+      items_per_page: none
+      region: right
+      weight: 0
+      uuid: 8c022726-d5ca-4c3a-a35d-c5dfa0457ebd
+      css_classes:
+        - ''
+      html_id: ''
+      css_styles: ''
+    6590bdd8-89f5-4e2b-bf71-4dc7658a611d:
+      id: 'facet_block:content_type'
+      label: 'Content type'
+      label_display: visible
+      provider: facets
+      context_mapping: {  }
+      region: left
+      weight: 0
+      uuid: 6590bdd8-89f5-4e2b-bf71-4dc7658a611d
+      css_classes:
+        - ''
+      html_id: ''
+      css_styles: ''
+  page_title: Search
+  layout: bs_2col_stacked
+  layout_settings:
+    label: ''
+    layout:
+      wrapper: div
+      classes:
+        row: row
+      add_layout_class: 1
+      attributes: ''
+    regions:
+      top:
+        wrapper: div
+        classes:
+          col-sm-12: col-sm-12
+        add_region_classes: 1
+        attributes: ''
+      left:
+        wrapper: div
+        classes:
+          col-sm-12: col-sm-12
+          col-md-3: col-md-3
+        add_region_classes: 1
+        attributes: class|pull-right
+      right:
+        wrapper: div
+        classes:
+          col-sm-12: col-sm-12
+          col-md-9: col-md-9
+        add_region_classes: 1
+        attributes: class|pull-left
+      bottom:
+        wrapper: div
+        classes:
+          col-sm-12: col-sm-12
+        add_region_classes: 1
+        attributes: ''
+  builder: standard
+  storage_type: page_manager
+  storage_id: site_search-panels_variant-0
+  css_classes:
+    - ''
+  html_id: ''
+  css_styles: ''
+page: site_search
+selection_criteria: {  }
+selection_logic: and
+static_context: {  }

html/modules/custom/op_ext_search/config/optional/views.view.site_search.yml
@@ -1,0 +1,426 @@
+langcode: en
+status: true
+dependencies:
+  config:
+    - field.storage.node.body
+    - search_api.index.default_index
+  module:
+    - search_api
+    - text
+_core:
+  default_config_hash: d_6tT5-HgPPHFGZtutKXVhPVZIA107ey7XpPGXGaYAQ
+id: site_search
+label: 'Site search'
+module: views
+description: ''
+tag: ''
+base_table: search_api_index_default_index
+base_field: search_api_id
+display:
+  default:
+    id: default
+    display_title: Default
+    display_plugin: default
+    position: 0
+    display_options:
+      title: 'Site search'
+      fields:
+        title:
+          id: title
+          table: search_api_datasource_default_index_entity_node
+          field: title
+          relationship: none
+          group_type: group
+          admin_label: ''
+          entity_type: node
+          plugin_id: search_api_field
+          label: ''
+          exclude: false
+          alter:
+            alter_text: false
+            text: ''
+            make_link: false
+            path: ''
+            absolute: false
+            external: false
+            replace_spaces: false
+            path_case: none
+            trim_whitespace: false
+            alt: ''
+            rel: ''
+            link_class: ''
+            prefix: ''
+            suffix: ''
+            target: ''
+            nl2br: false
+            max_length: 0
+            word_boundary: true
+            ellipsis: true
+            more_link: false
+            more_link_text: ''
+            more_link_path: ''
+            strip_tags: false
+            trim: false
+            preserve_tags: ''
+            html: false
+          element_type: h2
+          element_class: h3
+          element_label_type: ''
+          element_label_class: ''
+          element_label_colon: false
+          element_wrapper_type: ''
+          element_wrapper_class: ''
+          element_default_classes: false
+          empty: ''
+          hide_empty: false
+          empty_zero: false
+          hide_alter_empty: true
+          click_sort_column: value
+          type: string
+          settings:
+            link_to_entity: true
+          group_column: value
+          group_columns: {  }
+          group_rows: true
+          delta_limit: 0
+          delta_offset: 0
+          delta_reversed: false
+          delta_first_last: false
+          multi_type: separator
+          separator: ', '
+          field_api_classes: false
+          field_rendering: true
+          fallback_handler: search_api
+          fallback_options:
+            link_to_item: false
+            use_highlighting: false
+            multi_type: separator
+            multi_separator: ', '
+        body:
+          id: body
+          table: search_api_datasource_default_index_entity_node
+          field: body
+          relationship: none
+          group_type: group
+          admin_label: ''
+          entity_type: node
+          plugin_id: search_api_field
+          label: ''
+          exclude: true
+          alter:
+            alter_text: false
+            text: ''
+            make_link: false
+            path: ''
+            absolute: false
+            external: false
+            replace_spaces: false
+            path_case: none
+            trim_whitespace: false
+            alt: ''
+            rel: ''
+            link_class: ''
+            prefix: ''
+            suffix: ''
+            target: ''
+            nl2br: false
+            max_length: 0
+            word_boundary: true
+            ellipsis: true
+            more_link: false
+            more_link_text: ''
+            more_link_path: ''
+            strip_tags: false
+            trim: false
+            preserve_tags: ''
+            html: false
+          element_type: ''
+          element_class: ''
+          element_label_type: ''
+          element_label_class: ''
+          element_label_colon: false
+          element_wrapper_type: ''
+          element_wrapper_class: ''
+          element_default_classes: true
+          empty: ''
+          hide_empty: false
+          empty_zero: false
+          hide_alter_empty: true
+          click_sort_column: value
+          type: text_default
+          settings: {  }
+          group_column: value
+          group_columns: {  }
+          group_rows: true
+          delta_limit: 0
+          delta_offset: 0
+          delta_reversed: false
+          delta_first_last: false
+          multi_type: separator
+          separator: ', '
+          field_api_classes: false
+          field_rendering: false
+          fallback_handler: search_api
+          fallback_options:
+            link_to_item: false
+            use_highlighting: false
+            multi_type: separator
+            multi_separator: ', '
+        search_api_excerpt:
+          id: search_api_excerpt
+          table: search_api_index_default_index
+          field: search_api_excerpt
+          relationship: none
+          group_type: group
+          admin_label: ''
+          plugin_id: search_api
+          label: ''
+          exclude: false
+          alter:
+            alter_text: false
+            text: ''
+            make_link: false
+            path: ''
+            absolute: false
+            external: false
+            replace_spaces: false
+            path_case: none
+            trim_whitespace: false
+            alt: ''
+            rel: ''
+            link_class: ''
+            prefix: ''
+            suffix: ''
+            target: ''
+            nl2br: false
+            max_length: 0
+            word_boundary: true
+            ellipsis: true
+            more_link: false
+            more_link_text: ''
+            more_link_path: ''
+            strip_tags: false
+            trim: false
+            preserve_tags: ''
+            html: false
+          element_type: ''
+          element_class: ''
+          element_label_type: ''
+          element_label_class: ''
+          element_label_colon: false
+          element_wrapper_type: ''
+          element_wrapper_class: ''
+          element_default_classes: true
+          empty: '{{ body }}'
+          hide_empty: false
+          empty_zero: false
+          hide_alter_empty: true
+          link_to_item: false
+          use_highlighting: true
+          multi_type: separator
+          multi_separator: ', '
+      pager:
+        type: full
+        options:
+          offset: 0
+          items_per_page: 10
+          total_pages: null
+          id: 0
+          tags:
+            next: 'Next ›'
+            previous: '‹ Previous'
+            first: '« First'
+            last: 'Last »'
+          expose:
+            items_per_page: false
+            items_per_page_label: 'Items per page'
+            items_per_page_options: '5, 10, 25, 50'
+            items_per_page_options_all: false
+            items_per_page_options_all_label: '- All -'
+            offset: false
+            offset_label: Offset
+          quantity: 9
+      exposed_form:
+        type: basic
+        options:
+          submit_button: Search
+          reset_button: false
+          reset_button_label: Reset
+          exposed_sorts_label: 'Sort by'
+          expose_sort_order: true
+          sort_asc_label: Asc
+          sort_desc_label: Desc
+      access:
+        type: none
+        options: {  }
+      cache:
+        type: none
+        options: {  }
+      empty: {  }
+      sorts:
+        search_api_relevance:
+          id: search_api_relevance
+          table: search_api_index_default_index
+          field: search_api_relevance
+          relationship: none
+          group_type: group
+          admin_label: ''
+          plugin_id: search_api
+          order: DESC
+          expose:
+            label: ''
+            field_identifier: ''
+          exposed: false
+      arguments: {  }
+      filters:
+        search_api_fulltext:
+          id: search_api_fulltext
+          table: search_api_index_default_index
+          field: search_api_fulltext
+          relationship: none
+          group_type: group
+          admin_label: ''
+          plugin_id: search_api_fulltext
+          operator: or
+          value: ''
+          group: 1
+          exposed: true
+          expose:
+            operator_id: search_api_fulltext_op
+            label: ''
+            description: ''
+            use_operator: false
+            operator: search_api_fulltext_op
+            operator_limit_selection: false
+            operator_list: {  }
+            identifier: search_api_fulltext
+            required: false
+            remember: false
+            multiple: false
+            remember_roles:
+              authenticated: authenticated
+              anonymous: '0'
+              administrator: '0'
+              creator: '0'
+              reviewer: '0'
+              content_administrator: '0'
+              content_manager: '0'
+            expose_fields: false
+            placeholder: ''
+            searched_fields_id: search_api_fulltext_searched_fields
+          is_grouped: false
+          group_info:
+            label: ''
+            description: ''
+            identifier: ''
+            optional: true
+            widget: select
+            multiple: false
+            remember: false
+            default_group: All
+            default_group_multiple: {  }
+            group_items: {  }
+          parse_mode: terms
+          min_length: null
+          fields: {  }
+        search_api_language:
+          id: search_api_language
+          table: search_api_index_default_index
+          field: search_api_language
+          relationship: none
+          group_type: group
+          admin_label: ''
+          plugin_id: search_api_language
+          operator: in
+          value:
+            '***LANGUAGE_language_interface***': '***LANGUAGE_language_interface***'
+          group: 1
+          exposed: false
+          expose:
+            operator_id: ''
+            label: ''
+            description: ''
+            use_operator: false
+            operator: ''
+            operator_limit_selection: false
+            operator_list: {  }
+            identifier: ''
+            required: false
+            remember: false
+            multiple: false
+            remember_roles:
+              authenticated: authenticated
+            reduce: false
+          is_grouped: false
+          group_info:
+            label: ''
+            description: ''
+            identifier: ''
+            optional: true
+            widget: select
+            multiple: false
+            remember: false
+            default_group: All
+            default_group_multiple: {  }
+            group_items: {  }
+      style:
+        type: default
+        options:
+          grouping: {  }
+          row_class: ''
+          default_row_class: false
+      row:
+        type: fields
+      query:
+        type: views_query
+        options:
+          bypass_access: false
+          skip_access: false
+          preserve_facet_query_args: false
+      relationships: {  }
+      header:
+        result:
+          id: result
+          table: views
+          field: result
+          relationship: none
+          group_type: group
+          admin_label: ''
+          plugin_id: result
+          empty: false
+          content: '<h2 class="h4">Displaying @start to @end of @total items.</h2>'
+      footer: {  }
+      display_extenders: {  }
+    cache_metadata:
+      max-age: -1
+      contexts:
+        - 'languages:language_content'
+        - 'languages:language_interface'
+        - url
+        - url.query_args
+        - 'user.node_grants:view'
+      tags:
+        - 'config:field.storage.node.body'
+        - 'config:search_api.index.default_index'
+  block_1:
+    id: block_1
+    display_title: Block
+    display_plugin: block
+    position: 1
+    display_options:
+      display_extenders:
+        metatag_display_extender:
+          metatags: {  }
+          tokenize: false
+      block_description: 'Site search'
+    cache_metadata:
+      max-age: -1
+      contexts:
+        - 'languages:language_content'
+        - 'languages:language_interface'
+        - url
+        - url.query_args
+        - 'user.node_grants:view'
+      tags:
+        - 'config:field.storage.node.body'
+        - 'config:search_api.index.default_index'

commit 446c3af5c66ad238db66a0a94752510df1b9bda9

Issue #17: Add ::accessCheck() to entity query
Fixes issue introduced in b570dde.

html/modules/custom/op_ext_search/src/Plugin/Block/CustomSearchBlock.php
@@ -86,7 +86,7 @@ class CustomSearchBlock extends BlockBase implements ContainerFactoryPluginInter
     $facetsList = [];
 
     // Store Facets block list.
-    $facets = $this->entityTypeManager->getStorage('facets_facet')->getQuery()->execute();
+    $facets = $this->entityTypeManager->getStorage('facets_facet')->getQuery()->accessCheck(FALSE)->execute();
     foreach ($facets as $value) {
       $facetsLoad = $this->entityTypeManager->getStorage('facets_facet')->load($value);
 