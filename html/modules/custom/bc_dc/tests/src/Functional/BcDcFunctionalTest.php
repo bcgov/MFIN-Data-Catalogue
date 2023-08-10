9c34910fffd69d8c3eb2ae7933580409bcf4c424
Liam Morland <liam@openplus.ca>
2023-08-10 14:26:12 -0400

    Issue bcbb-18: Test book features

--- html/modules/custom/bc_dc/tests/src/Functional/BcDcFunctionalTest.php
+++ html/modules/custom/bc_dc/tests/src/Functional/BcDcFunctionalTest.php
@@ -425,6 +425,71 @@ class BcDcFunctionalTest extends BrowserTestBase {
     $this->click('div.flag-bookmark.action-flag > a');
     // Bookmarked by 2.
     $this->assertSession()->elementExists('xpath', '//a[*[@class = "title"][text() = "Remove bookmark"]][*[@class = "count"][text() = "Bookmarked by 2 people"]]');
+
+    // Test book module.
+    //
+    // Create a Book as admin.
+    $this->drupalLogin($this->rootUser);
+    $this->drupalGet('node/add/book');
+    $this->assertSession()->statusCodeEquals(200);
+    $edit_book = [
+      'edit-title-0-value' => 'Test Book ' . $this->randomString(),
+      'edit-book-bid' => 'new',
+    ];
+    $this->submitForm($edit_book, 'Save');
+    $text = $this->assertSession()->elementExists('xpath', '//h1')->getText();
+    $this->assertStringContainsString($edit_book['edit-title-0-value'], $text);
+    $book_url = $this->getUrl();
+    // Create child page as Test Data administrator.
+    $this->drupalLogin($this->users['Test Data administrator']);
+    $this->drupalGet($book_url);
+    $this->clickLink('Add child page');
+    $this->assertSession()->statusCodeEquals(200);
+    $edit_child = [
+      'edit-title-0-value' => 'Test Book Child Page ' . $this->randomString(),
+      'edit-body-0-summary' => 'Test Book Child Summary ' . $this->randomString(),
+    ];
+    $this->submitForm($edit_child, 'Save');
+    $text = $this->assertSession()->elementExists('xpath', '//h1')->getText();
+    $this->assertStringContainsString($edit_child['edit-title-0-value'], $text);
+    $child_url = $this->getUrl();
+    // Create grandchild.
+    $this->clickLink('Add child page');
+    $this->assertSession()->statusCodeEquals(200);
+    $edit = [
+      'edit-title-0-value' => 'Test Book Grandchild Page ' . $this->randomString(),
+    ];
+    $this->submitForm($edit, 'Save');
+    $text = $this->assertSession()->elementExists('xpath', '//h1')->getText();
+    $this->assertStringContainsString($edit['edit-title-0-value'], $text);
+    // Book title appears in breadcrumbs.
+    $args = [
+      ':title' => $edit_book['edit-title-0-value'],
+    ];
+    $xpath = $this->assertSession()->buildXPathQuery('//nav/ol[@class = "breadcrumb"]/li/a[text() = :title]', $args);
+    $this->assertSession()->elementExists('xpath', $xpath);
+    // Main book page.
+    // Summary appears in list of child pages.
+    $this->drupalGet($book_url);
+    $args = [
+      ':summary' => $edit_child['edit-body-0-summary'],
+    ];
+    $xpath = $this->assertSession()->buildXPathQuery('//nav[@class = "book-navigation"]/ul/li/div[contains(@class, "summary")][text() = :summary]', $args);
+    $this->assertSession()->elementExists('xpath', $xpath);
+    // Book traversal links.
+    $args = [
+      ':title' => $edit_child['edit-title-0-value'],
+    ];
+    $xpath = $this->assertSession()->buildXPathQuery('//nav[@class = "book-navigation"]/ul[@aria-label = "Document navigation"]/li/a[@title = "Go to next page"][contains(text(), :title)]', $args);
+    $this->assertSession()->elementExists('xpath', $xpath);
+    // Default "Book traversal links" header is not present.
+    $this->assertSession()->pageTextNotContains('Book traversal links');
+    // Printer-friendly version.
+    $this->assertSession()->elementExists('xpath', '//div[@class = "node__links"]/ul/li/a[text() = "Printer-friendly version"]');
+    // Child page.
+    $this->drupalGet($child_url);
+    // Child page does not have list of child pages.
+    $this->assertSession()->elementNotExists('xpath', '//nav[@class = "book-navigation"]/ul[not(@aria-label)]');
   }
 
   /**
