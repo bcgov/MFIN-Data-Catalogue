bcgov/MFIN-Data-Catalogue

commit 56c7cefa5df11ca1268321a27c1967a63f58524e

Issue #26: Send notifications when updating data_set nodes

html/modules/custom/bc_dc/tests/src/Functional/BcDcFunctionalTest.php
@@ -37,6 +37,7 @@ class BcDcFunctionalTest extends BrowserTestBase {
    */
   protected static $modules = [
     'bc_dc',
+    'dblog',
   ];
 
   /**
@@ -356,6 +357,25 @@ class BcDcFunctionalTest extends BrowserTestBase {
     $this->assertSession()->elementExists('xpath', '//table[contains(@class, "dc-dashboard-table-bookmarks")]//tr
       [td/span[@class = "updated"][text() = "Updated:"]]
       [td/a[@href = "/node/2/build"]]');
+
+    // Examine logs to check that update notification emails would have been
+    // sent to users who bookmarked the updated data_set.
+    $options = [
+      'query' => ['type' => ['bc_dc', 'message_gcnotify']],
+    ];
+    $this->drupalGet('admin/reports/dblog', $options);
+    $this->assertSession()->elementExists('xpath', '//div[contains(@class, "view-watchdog")]/div/table/tbody/tr/td/a[text() = "Sent message to 1 users when updating data_set 2."]');
+    // Get from @title the request that would have been sent to GC Notify.
+    $element = $this->xpath('//div[contains(@class, "view-watchdog")]/div/table/tbody/tr/td/a[contains(@title, "A dataset you have bookmarked has been updated")]');
+    $element = reset($element);
+    $title = $element->getAttribute('title');
+    preg_match('/^GC Notify disabled[^:]+: (.+)/', $title, $matches);
+    $gcnotify_request = json_decode($matches[1]);
+    // Run tests on the request.
+    $this->assertEquals('A dataset you have bookmarked has been updated', $gcnotify_request->rows[1][1]);
+    $this->assertMatchesRegularExpression(',The following dataset has been updated:
+' . preg_quote($data_set_title) . '
+https?://[^/]+/node/2,', htmlspecialchars_decode($gcnotify_request->rows[1][2]));
 
     // The bookmark field_last_viewed_date gets updated when visiting a page.
     //

html/modules/custom/bc_dc/bc_dc.info.yml
@@ -18,6 +18,7 @@ dependencies:
   - entity_reference_revisions:entity_reference_revisions
   - flag:flag
   - form_mode_control:form_mode_control
+  - message_gcnotify:message_gcnotify
   - op_ext_search:op_ext_search
   - page_manager:page_manager
   - page_manager:page_manager_ui

html/modules/custom/bc_dc/bc_dc.module
@@ -8,6 +8,7 @@
 use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Render\Element;
+use Drupal\message_gcnotify\Service\GcNotifyApiService;
 use Drupal\node\NodeInterface;
 
 /**
@@ -121,4 +122,82 @@ function bc_dc_node_view(array &$build, NodeInterface $entity, EntityViewDisplay
   // Record that the node has been viewed. Update field_last_viewed_date to now.
   $bookmark_flagging->set('field_last_viewed_date', date('Y-m-d\TH:i:s'));
   $bookmark_flagging->save();
+}
+
+/**
+ * Implements hook_ENTITY_TYPE_update() for node.
+ */
+function bc_dc_node_update(NodeInterface $entity): void {
+  // Send a notification to all users who have bookmarked this data_set and who
+  // last viewed it before the modified_date.
+  //
+  // Act only on data_set nodes.
+  if ($entity->bundle() !== 'data_set') {
+    return;
+  }
+
+  // If field_modified_date is not made greater than it was, do nothing.
+  $old_modified_date = $entity->original->field_modified_date->value;
+  $new_modified_date = $entity->field_modified_date->value;
+  if ($new_modified_date <= $old_modified_date) {
+    return;
+  }
+
+  // Load an array of all bookmarks.
+  $flag_service = \Drupal::service('flag');
+  $bookmark_flag = $flag_service->getFlagById('bookmark');
+  /** @var \Drupal\flag\FlaggingInterface[] $bookmark_flaggings Array of flaggings. */
+  $bookmark_flaggings = $flag_service->getEntityFlaggings($bookmark_flag, $entity);
+
+  // Create an array of users who need to be notified of the change.
+  /** @var string[] $send_to Array of email addresses. */
+  $send_to = [];
+  // Add to the list if the data set was modified after the last viewed date.
+  // We compare dates because field_modified_date might not be set to now, but
+  // some time in the past when it was know that an update was made.
+  foreach ($bookmark_flaggings as $bookmark_flagging) {
+    if ($new_modified_date > $bookmark_flagging->get('field_last_viewed_date')->value) {
+      $email = $bookmark_flagging->getOwner()->getEmail();
+      if ($email) {
+        $send_to[] = $email;
+      }
+    }
+  }
+
+  // No recipients.
+  if (!$send_to) {
+    $context = [
+      '@nid' => $entity->id(),
+    ];
+    \Drupal::logger('bc_dc')->notice('No messages to send when updating data_set @nid.', $context);
+    return;
+  }
+
+  // Send the notifications.
+  //
+  // Message subject.
+  $subject = t('A dataset you have bookmarked has been updated');
+  // Message body.
+  $context = [
+    '@title' => $entity->getTitle(),
+    '@link' => $entity->toUrl('canonical', ['absolute' => TRUE])->toString(),
+  ];
+  $body = t('The following dataset has been updated:
+@title
+@link', $context);
+  // Send the message and log the result.
+  $success = GcNotifyApiService::sendMessage($send_to, $subject, $body);
+  if ($success) {
+    $context = [
+      '@count' => count($send_to),
+      '@nid' => $entity->id(),
+    ];
+    \Drupal::logger('bc_dc')->notice('Sent message to @count users when updating data_set @nid.', $context);
+  }
+  else {
+    $context = [
+      '@nid' => $entity->id(),
+    ];
+    \Drupal::logger('bc_dc')->error('Failed sending message when updating data_set @nid.', $context);
+  }
 }

commit 200ffec01f1cee30dc4603c4f08714763824878c

Issue #26: Prevent less-than characters from being returned by randomString()
This avoids test errors when the less-than character is interpreted as the start
of an HTML tag.

Fixes issue introduced in 56c7cef.

html/modules/custom/bc_dc/tests/src/Functional/BcDcFunctionalTest.php
@@ -60,6 +60,18 @@ class BcDcFunctionalTest extends BrowserTestBase {
   }
 
   /**
+   * {@inheritdoc}
+   *
+   * Same as parent except that it will not return a less-than character, which
+   * can be interpreted as the start of an HTML tag.
+   */
+  public function randomString($length = 8) {
+    $string = parent::randomString($length);
+    $string = str_replace('<', 'a', $string);
+    return $string;
+  }
+
+  /**
    * Create a user with roles.
    */
   protected function createTestUser(string $name, array $roles = []): User|false {