bcgov/MFIN-Data-Catalogue

commit c5cfe930eab4aef1b05b0912acf87eff5cadfdd3

Issue #26: Update field_last_viewed_date when visiting a bookmarked data_set

html/modules/custom/bc_dc/bc_dc.module
@@ -5,8 +5,10 @@
  * Module file.
  */
 
+use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Render\Element;
+use Drupal\node\NodeInterface;
 
 /**
  * Implements hook_preprocess_HOOK().
@@ -95,4 +97,28 @@ function bc_dc_entity_display_build_alter(array &$build, array $context): void {
       ] + $build[$field_name];
     }
   }
+}
+
+/**
+ * Implements hook_ENTITY_TYPE_view() for node.
+ */
+function bc_dc_node_view(array &$build, NodeInterface $entity, EntityViewDisplayInterface $display, string $view_mode): void {
+  // Act only on data_set nodes.
+  if ($entity->bundle() !== 'data_set') {
+    return;
+  }
+
+  // Load the bookmark for this user for this node.
+  $flag_service = \Drupal::service('flag');
+  $bookmark_flag = $flag_service->getFlagById('bookmark');
+  $bookmark_flagging = $flag_service->getFlagging($bookmark_flag, $entity);
+
+  // Return if no bookmark on this node by the current user.
+  if (!$bookmark_flagging) {
+    return;
+  }
+
+  // Record that the node has been viewed. Update field_last_viewed_date to now.
+  $bookmark_flagging->set('field_last_viewed_date', date('Y-m-d\TH:i:s'));
+  $bookmark_flagging->save();
 }

html/modules/custom/bc_dc/tests/src/Functional/BcDcFunctionalTest.php
@@ -345,6 +345,26 @@ class BcDcFunctionalTest extends BrowserTestBase {
     $this->assertSession()->statusCodeEquals(200);
     $this->assertSession()->elementExists('xpath', '//table[contains(@class, "dc-dashboard-table-mydatasets")]//tr/td[text() = "No data sets to show."]');
 
+    // The bookmark field_last_viewed_date gets updated when visiting a page.
+    //
+    // Get needed services.
+    $flagService = \Drupal::service('flag');
+    $bookmark_flag = $flagService->getFlagById('bookmark');
+    $bookmark_flagging = $flagService->getFlagging($bookmark_flag, $data_set);
+    // Set the field_last_viewed_date to yesterday.
+    $date_yesterday = (new \DateTime('yesterday'))->format('Y-m-d\TH:i:s');
+    $bookmark_flagging->set('field_last_viewed_date', $date_yesterday)->save();
+    // Visit the page to update the last-visited time.
+    $this->drupalGet('node/2');
+    // The field_last_viewed_date should now be later.
+    $field_last_viewed_date = $flagService->getFlagging($bookmark_flag, $data_set)->get('field_last_viewed_date')->value;
+    // Ensure comparisons are between ISO dates.
+    $this->assertMatchesRegularExpression('/^\d\d\d\d-\d\d-\d\dT/', $date_yesterday);
+    $this->assertMatchesRegularExpression('/^\d\d\d\d-\d\d-\d\dT/', $field_last_viewed_date);
+    // Check that field_last_viewed_date is now greater than what it was set to.
+    $this->assertGreaterThan(1, 2);
+    $this->assertGreaterThan($date_yesterday, $field_last_viewed_date);
+
     // Data set landing page.
     $this->drupalGet('data-set');
     $this->assertSession()->statusCodeEquals(200);

commit 934ca2207227905cb04ffdc8913c7e2914868751

Issue #26: Convert hook_ENTITY_TYPE_view() into block bc_dc_page_view_tracker
Due to caching, the hook does not run on every page load.

Fixes issue introduced in c5cfe93.

html/modules/custom/bc_dc/bc_dc.module
@@ -5,7 +5,6 @@
  * Module file.
  */
 
-use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\Element;
@@ -96,27 +95,6 @@ function bc_dc_entity_display_build_alter(array &$build, array $context): void {
         '0' => $markup,
       ] + $build[$field_name];
     }
-  }
-}
-
-/**
- * Implements hook_ENTITY_TYPE_view() for node.
- */
-function bc_dc_node_view(array &$build, NodeInterface $entity, EntityViewDisplayInterface $display, string $view_mode): void {
-  // Act only on data_set nodes.
-  if ($entity->bundle() !== 'data_set') {
-    return;
-  }
-
-  // If there is a bookmark by the user, record that the node has been viewed.
-  // Load the bookmark for this user for this node.
-  $flag_service = \Drupal::service('flag');
-  $bookmark_flag = $flag_service->getFlagById('bookmark');
-  $bookmark_flagging = $flag_service->getFlagging($bookmark_flag, $entity);
-  // Update field_last_viewed_date to now.
-  if ($bookmark_flagging) {
-    $bookmark_flagging->set('field_last_viewed_date', date('Y-m-d\TH:i:s'));
-    $bookmark_flagging->save();
   }
 }
 

html/modules/custom/bc_dc/src/Plugin/Block/PageViewTracker.php
@@ -1,0 +1,94 @@
+<?php
+
+namespace Drupal\bc_dc\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\flag\FlagService;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Empty block exists to runs code on each page view.
+ *
+ * Due to caching, hooks like hook_ENTITY_TYPE_view() only run when a page is
+ * built, not on every page view. This un-cached block gets built every time,
+ * allowing running artibrary code each time. Empty block; nothing is displayed.
+ *
+ * If the node is bookmarked by the user, record that it has been viewed.
+ *
+ * @Block(
+ *   id = "bc_dc_page_view_tracker",
+ *   admin_label = @Translation("Page view tracker"),
+ *   category = @Translation("BC Data Catalog"),
+ * )
+ */
+class PageViewTracker extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Constructor.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $currentRouteMatch
+   *   The current_route_match service.
+   * @param \Drupal\flag\FlagService $flagService
+   *   The flag service.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    protected RouteMatchInterface $currentRouteMatch,
+    protected FlagService $flagService,
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): static {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_route_match'),
+      $container->get('flag'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    $entity = $this->currentRouteMatch->getParameter('node');
+
+    // If the node exists and is bookmarked by the user, update last viewed.
+    if ($entity) {
+      // Load the bookmark for this user for this node.
+      $bookmark_flag = $this->flagService->getFlagById('bookmark');
+      $bookmark_flagging = $this->flagService->getFlagging($bookmark_flag, $entity);
+      // Update field_last_viewed_date to now.
+      if ($bookmark_flagging) {
+        $bookmark_flagging->set('field_last_viewed_date', date('Y-m-d\TH:i:s'));
+        $bookmark_flagging->save();
+      }
+    }
+
+    // Return an empty block.
+    $build = [
+      // Disable cache so this will run every time.
+      '#cache' => [
+        'max-age' => 0,
+      ],
+    ];
+
+    return $build;
+  }
+
+}