8640e706614adf722dffb501c5e7f3faca2eae36
Jennifer Houde <jenniferhoude@Jennifers-MacBook-Pro.local>
2023-10-10 14:44:18 -0400

    Updating controller from feedback

--- html/modules/custom/bc_dc/src/Controller/BcDcCreateFileController.php
+++ html/modules/custom/bc_dc/src/Controller/BcDcCreateFileController.php
@@ -15,6 +15,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
  */
 class BcDcCreateFileController extends ControllerBase {
 
+  const FILE_EXTENSION = array('csv', 'xlsx');
   /**
    * Returns a downloadable file.
    */
@@ -23,79 +24,77 @@ class BcDcCreateFileController extends ControllerBase {
     $alias = \Drupal::service('path_alias.manager')->getAliasByPath('/node/' . $nid);
     $path = str_replace("/data-set/", "", $alias);
     $allowedExt = ['csv', 'xlsx'];
-    if (in_array($param, $allowedExt)) {
-      $entity = $this->entityTypeManager()->getStorage('node')->load($nid);
-      $paragraph_field_items = $entity->get('field_columns')->referencedEntities();
-      foreach ($paragraph_field_items as $paragraph) {
-        // Get the translation.
-        $paragraph = \Drupal::service('entity.repository')->getTranslationFromContext($paragraph);
-      }
-      $sheetData = [
-            ['column_allowed_values',
-              'column_data_quality',
-              'column_description',
-              'column_name',
-              'column_size',
-              'column_transformations',
-              'provenance_field_name',
-              'provenance_field_number',
-              'provenance_field_question',
-              'provenance_form_name',
-              'provenance_form_number',
-            ],
-            [$paragraph->get('field_column_allowed_values')->value,
-              $paragraph->get('field_column_data_quality')->value,
-              $paragraph->get('field_column_description')->value,
-              $paragraph->get('field_column_name')->value,
-              $paragraph->get('field_column_size')->value,
-              $paragraph->get('field_column_transformations')->value,
-              $paragraph->get('field_provenance_field_name')->value,
-              $paragraph->get('field_provenance_field_number')->value,
-              $paragraph->get('field_provenance_field_question')->value,
-              $paragraph->get('field_provenance_form_name')->value,
-              $paragraph->get('field_provenance_form_number')->value,
-            ],
-      ];
-
-      $spreadsheet = new Spreadsheet();
-      switch ($param) {
-        case 'csv':
-          $filename = $path . '_ID_' . $nid . '.xlsx';
-          // Save to file.
-          $worksheets = new Worksheet($spreadsheet, "Sheet 1");
-          $spreadsheet->addSheet($worksheets, 0);
-          $worksheets->fromArray($sheetData);
-          foreach ($worksheets as $worksheet) {
-            foreach ($worksheet->getColumnIterator() as $column) {
-              $worksheet->getColumnDimension($column->getColumnIndex())->setAutoSize(TRUE);
-            }
-          }
-          $writer = new Xlsx($spreadsheet);
-          $directory = 'public://data_dictionary/' . $filename;
-          $writer->save($directory);
-
-        case 'xlsx':
-          $filename = $path . '_ID_' . $nid . '.csv';
-          $mySpreadsheet = new Spreadsheet();
-          $worksheets = new Worksheet($mySpreadsheet, "Sheet 1");
-          $mySpreadsheet->addSheet($worksheets, 0);
-          $worksheets->fromArray($sheetData);
-          foreach ($worksheets as $worksheet) {
-            foreach ($worksheet->getColumnIterator() as $column) {
-              $worksheet->getColumnDimension($column->getColumnIndex())->setAutoSize(TRUE);
-            }
-          }
-          $writer = new Csv($mySpreadsheet);
-          $directory = 'public://data_dictionary/' . $filename;
-          $writer->setDelimiter(',');
-          $writer->setEnclosure('');
-          $writer->setLineEnding("\r\n");
-          $writer->setSheetIndex(0);
-          $writer->save($directory);
-      }
-    }
-    else {
+    if(!in_array($param, FILE_EXTENSION, TRUE)) {
       throw new NotFoundHttpException();
+    }
+    $entity = $this->entityTypeManager()->getStorage('node')->load($nid);
+    $paragraph_field_items = $entity->get('field_columns')->referencedEntities();
+    foreach ($paragraph_field_items as $paragraph) {
+      // Get the translation.
+      $paragraph = \Drupal::service('entity.repository')->getTranslationFromContext($paragraph);
+    }
+    $sheetData = [
+      ['column_allowed_values',
+        'column_data_quality',
+        'column_description',
+        'column_name',
+        'column_size',
+        'column_transformations',
+        'provenance_field_name',
+        'provenance_field_number',
+        'provenance_field_question',
+        'provenance_form_name',
+        'provenance_form_number',
+      ],
+      [$paragraph->get('field_column_allowed_values')->value,
+        $paragraph->get('field_column_data_quality')->value,
+        $paragraph->get('field_column_description')->value,
+        $paragraph->get('field_column_name')->value,
+        $paragraph->get('field_column_size')->value,
+        $paragraph->get('field_column_transformations')->value,
+        $paragraph->get('field_provenance_field_name')->value,
+        $paragraph->get('field_provenance_field_number')->value,
+        $paragraph->get('field_provenance_field_question')->value,
+        $paragraph->get('field_provenance_form_name')->value,
+        $paragraph->get('field_provenance_form_number')->value,
+      ],
+    ];
+
+    $spreadsheet = new Spreadsheet();
+    switch ($param) {
+      case 'xlsx':
+        $filename = $path . '_ID_' . $nid . '.xlsx';
+        // Save to file.
+        $worksheets = new Worksheet($spreadsheet, "Sheet 1");
+        $spreadsheet->addSheet($worksheets, 0);
+        $worksheets->fromArray($sheetData);
+        foreach ($worksheets as $worksheet) {
+          foreach ($worksheet->getColumnIterator() as $column) {
+            $worksheet->getColumnDimension($column->getColumnIndex())->setAutoSize(TRUE);
+          }
+        }
+        $writer = new Xlsx($spreadsheet);
+        $directory = 'public://data_dictionary/' . $filename;
+        $writer->save($directory);
+
+      case 'csv':
+        $filename = $path . '_ID_' . $nid . '.csv';
+        $mySpreadsheet = new Spreadsheet();
+        $worksheets = new Worksheet($mySpreadsheet, "Sheet 1");
+        $mySpreadsheet->addSheet($worksheets, 0);
+        $worksheets->fromArray($sheetData);
+        foreach ($worksheets as $worksheet) {
+          foreach ($worksheet->getColumnIterator() as $column) {
+            $worksheet->getColumnDimension($column->getColumnIndex())->setAutoSize(TRUE);
+          }
+        }
+        $writer = new Csv($mySpreadsheet);
+        $directory = 'public://data_dictionary/' . $filename;
+        $writer->setDelimiter(',');
+        $writer->setEnclosure('');
+        $writer->setLineEnding("\r\n");
+        $writer->setSheetIndex(0);
+        $writer->save($directory);
     }
 
     header("Content-Transfer-Encoding: Binary");
