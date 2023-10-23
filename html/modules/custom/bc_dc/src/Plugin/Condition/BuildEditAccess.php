24b5fd6b9c1ffffc67bb82c0ee28d816097d4b8b
Liam Morland <liam@openplus.ca>
2023-10-23 15:11:04 -0400

    Issue #158: Create condition plugin bc_dc_build_edit_access

---
+++ html/modules/custom/bc_dc/src/Plugin/Condition/BuildEditAccess.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\bc_dc\Plugin\Condition;
+
+use Drupal\Core\Condition\ConditionPluginBase;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\Url;
+
+/**
+ * Provides a condition for whether the user may edit a node.
+ *
+ * @Condition(
+ *   id = "bc_dc_build_edit_access",
+ *   label = @Translation("Build page access"),
+ *   context_definitions = {
+ *     "node" = @ContextDefinition(
+ *       "entity:node",
+ *       required = TRUE,
+ *       label = @Translation("Node")
+ *     )
+ *   }
+ * )
+ */
+class BuildEditAccess extends ConditionPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function evaluate(): bool {
+    $entity = $this->getContextValue('node');
+
+    if (!$entity) {
+      return FALSE;
+    }
+
+    // Give access based on whether the user has access to the edit path for
+    // data_set_description.
+    $route_parameters = [
+      'node' => $entity->id(),
+    ];
+    $options = [
+      'query' => [
+        'display' => 'data_set_description',
+      ],
+    ];
+    $access = Url::fromRoute('entity.node.edit_form', $route_parameters, $options)->access();
+
+    // Support negation.
+    return $access xor $this->isNegated();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function summary(): TranslatableMarkup {
+    return $this->t('User has access to edit the metadata record description.');
+  }
+
+}
