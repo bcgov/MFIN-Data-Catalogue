bcgov/MFIN-Data-Catalogue

commit ea2f80581310216b8a98baccb19361602a709faf

Download data dictionary as csv or xlsx #178  (#187)
Creating controller for data catalogue to create link for csv and excel and to download file when clicking on url, using hook to render link on upload page and using twig to render links on node view page. Functional tests created and passed.
---------
Co-authored-by: Jennifer Houde <jenniferhoude@Jennifers-MacBook-Pro.local>
Co-authored-by: Liam Morland <liam@openplus.ca>

html/modules/custom/bc_dc/src/Controller/BcDcCreateFileController.php
@@ -1,0 +1,128 @@
+<?php
+
+namespace Drupal\bc_dc\Controller;
+
+use Drupal\bc_dc\Form\BcDcAddColumnsForm;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityRepositoryInterface;
+use Drupal\Core\File\FileSystemInterface;
+use Drupal\node\NodeInterface;
+use Drupal\path_alias\AliasManagerInterface;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Writer\Csv;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ * Create a file in csv or xlsx.
+ */
+class BcDcCreateFileController extends ControllerBase {
+
+  const SUPPORTED_EXTENSIONS = [
+    'csv',
+    'xlsx',
+  ];
+
+  /**
+   * Create a BcDcCreateFileController instance.
+   *
+   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entityRepository
+   *   The entity.repository service.
+   * @param \Drupal\Core\File\FileSystemInterface $fileSystem
+   *   The file_system service.
+   * @param \Drupal\path_alias\AliasManagerInterface $pathAliasManager
+   *   The path_alias.manager service.
+   */
+  public function __construct(
+    protected EntityRepositoryInterface $entityRepository,
+    protected FileSystemInterface $fileSystem,
+    protected AliasManagerInterface $pathAliasManager,
+  ) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container): object {
+    return new static(
+      $container->get('entity.repository'),
+      $container->get('file_system'),
+      $container->get('path_alias.manager'),
+    );
+  }
+
+  /**
+   * Generate the download file and serves it to the browser.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   The node.
+   * @param string $format
+   *   The format of the file to serve.
+   *
+   * @return Symfony\Component\HttpFoundation\BinaryFileResponse
+   *   The file to download.
+   */
+  public function createFile(NodeInterface $node, string $format): BinaryFileResponse {
+    // Not Found if $format is not a supported file extension.
+    if (!in_array($format, static::SUPPORTED_EXTENSIONS, TRUE)) {
+      throw new NotFoundHttpException();
+    }
+    $fields = BcDcAddColumnsForm::getDataSetFields();
+    $results = [$fields];
+    $paragraph_field_items = $node->get('field_columns')->referencedEntities();
+    foreach ($paragraph_field_items as $paragraph) {
+      // Get the translation.
+      $paragraph = $this->entityRepository->getTranslationFromContext($paragraph);
+      $rows = [];
+      foreach ($fields as $field) {
+        $row = $paragraph->get('field_' . $field)->value;
+        $rows[] = $row;
+      }
+      $results[] = $rows;
+    }
+
+    // Make a filename like "node-file-path_ID_12.csv".
+    $node_path = $this->pathAliasManager->getAliasByPath('/node/' . $node->id());
+    $node_path = basename($node_path);
+    $filename = $node_path . '_ID_' . $node->id() . '.' . $format;
+
+    // Create a file path to the temp directory. PhpSpreadsheet does not work
+    // with stream wrappers.
+    $file_path = $this->fileSystem->getTempDirectory() . '/' . $filename;
+
+    // Generate the spreadsheet object and save to $file_path.
+    $spreadsheet = new Spreadsheet();
+    $worksheets = new Worksheet($spreadsheet);
+    $spreadsheet->addSheet($worksheets);
+    $worksheets->fromArray($results);
+    // Remove first blank worksheet.
+    $sheetIndex = $spreadsheet->getIndex(
+      $spreadsheet->getSheetByName('Worksheet')
+    );
+    $spreadsheet->removeSheetByIndex($sheetIndex);
+
+    switch ($format) {
+      case 'xlsx':
+        $writer = new Xlsx($spreadsheet);
+        break;
+
+      case 'csv':
+        $writer = new Csv($spreadsheet);
+        $writer->setDelimiter(',');
+        $writer->setEnclosure('"');
+        $writer->setLineEnding("\r\n");
+        $writer->setSheetIndex(0);
+        break;
+    }
+    $writer->save($file_path);
+
+    // Cause the browser to save the file with the specified filename.
+    header('Content-Disposition: attachment; filename="' . $filename . '"');
+    // Serve the file.
+    return new BinaryFileResponse($file_path, 200);
+  }
+
+}

html/modules/custom/bc_dc/bc_dc.module
@@ -5,8 +5,11 @@
  * Module file.
  */
 
+use Drupal\bc_dc\Controller\BcDcCreateFileController;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\FieldableEntityInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
 use Drupal\Core\Render\Element;
 use Drupal\message_gcnotify\Service\GcNotifyApiService;
 use Drupal\node\NodeInterface;
@@ -275,4 +278,45 @@ function bc_dc_cron(): void {
   // Send review reminders.
   $reviewReminder = \Drupal::service('bc_dc.review_reminder');
   $reviewReminder->sendRemindersToAllUsers();
+}
+
+/**
+ * Implements hook_form_FORM_ID_alter().
+ */
+function bc_dc_form_bc_dc_add_columns_form_alter(array &$form, FormStateInterface $form_state, string $form_id): void {
+  $nid = $form_state->get('nid');
+  $entity = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
+  $data_dictionary_download_links = bc_dc_get_data_dictionary_download_links($entity);
+  if ($data_dictionary_download_links) {
+    $form['data_dictionary'] = $data_dictionary_download_links;
+    $form['data_dictionary']['#weight'] = 0;
+  }
+}
+
+/**
+ * Return a renderable list of links to download column import files.
+ *
+ * @param \Drupal\Core\Entity\FieldableEntityInterface $entity
+ *   The entity.
+ *
+ * @return array|null
+ *   A Drupal render array or NULL if there are no columns.
+ */
+function bc_dc_get_data_dictionary_download_links(FieldableEntityInterface $entity): ?array {
+  if (!$entity->get('field_columns')->referencedEntities()) {
+    return NULL;
+  }
+
+  $items = [];
+  foreach (BcDcCreateFileController::SUPPORTED_EXTENSIONS as $format) {
+    $items[] = Link::createFromRoute(
+      t('Download current data dictionary file to @format', ['@format' => strtoupper($format)]),
+      'bc_dc.create_columns_file',
+      ['node' => $entity->id(), 'format' => $format],
+    );
+  }
+  return [
+    '#theme' => 'item_list',
+    '#items' => $items,
+  ];
 }

html/modules/custom/bc_dc/bc_dc.routing.yml
@@ -27,3 +27,15 @@ bc_dc.data_set_edit_add_columns:
     parameters:
       node:
         type: entity:node
+
+bc_dc.create_columns_file:
+  path: '/node/{node}/download/columns/{format}'
+  defaults:
+    _controller: '\Drupal\bc_dc\Controller\BcDcCreateFileController::createFile'
+    _title: 'Generate file from columns'
+  requirements:
+    _permission: 'access content'
+  options:
+    parameters:
+      node:
+        type: entity:node

html/modules/custom/bc_dc/tests/src/Functional/BcDcFunctionalTest.php
@@ -536,6 +536,16 @@ https?://[^/]+/node/2)', htmlspecialchars_decode($gcnotify_request->rows[1][2]))
     // Page has ISO dates.
     $this->isoDateTest();
 
+    // Anonymous has access to download csv for Metadata record when file has
+    // been uploaded.
+    $this->drupalGet('node/2/download/columns/csv');
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->responseContains('Name');
+    // Anonymous has access to download xlsx for Metadata record when file has
+    // been uploaded.
+    $this->drupalGet('node/2/download/columns/xlsx');
+    $this->assertSession()->statusCodeEquals(200);
+
     // Test adding bookmarks.
     $this->drupalLogin($this->users['Test Data catalogue user']);
     $this->drupalGet('node/2');

html/themes/custom/dc_theme/dc_theme.theme
@@ -68,6 +68,9 @@ function dc_theme_preprocess_node__data_set__full(array &$variables): void {
     $name_parts[] = $information_schedule_term->label();
     $variables['field_information_schedule_display_name'] = implode(': ', $name_parts);
   }
+
+  // Data dictionary download links.
+  $variables['data_dictionary_download_links'] = bc_dc_get_data_dictionary_download_links($variables['node']);
 }
 
 /**

html/themes/custom/dc_theme/templates/node/node--data-set--full.html.twig
@@ -194,6 +194,7 @@
       {% if node.field_modified_date and not node.field_modified_date.isempty %}
       <hr>
       <h2>{{ 'Data dictionary'|t }}</h2>
+        {{ data_dictionary_download_links }}
         {{ node.field_columns|view }}
       {% endif %}
 

commit 266a397328737eebda63b6e960e610a53baa964f

Issue #204: Delete data dictionary download files after download
Fixes issue introduced in ea2f805.

html/modules/custom/bc_dc/src/Controller/BcDcCreateFileController.php
@@ -122,7 +122,9 @@ class BcDcCreateFileController extends ControllerBase {
     // Cause the browser to save the file with the specified filename.
     header('Content-Disposition: attachment; filename="' . $filename . '"');
     // Serve the file.
-    return new BinaryFileResponse($file_path, 200);
+    $response = new BinaryFileResponse($file_path, 200);
+    $response->deleteFileAfterSend();
+    return $response;
   }
 
 }