5f0790073579f6ddab9044ec97cf24966bfb377d
Liam Morland <liam@openplus.ca>
2023-12-14 12:24:49 -0500

    Issue #215: Create abstract class BcDcButtonBase

--- html/modules/custom/bc_dc/src/Plugin/views/field/BcDcDelete.php
+++ html/modules/custom/bc_dc/src/Plugin/views/field/BcDcDelete.php
@@ -2,91 +2,24 @@
 
 namespace Drupal\bc_dc\Plugin\views\field;
 
-use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Link;
-use Drupal\Core\Url;
-use Drupal\views\Plugin\views\field\FieldPluginBase;
-use Drupal\views\ResultRow;
-
 /**
- * Field handler to delete button .
+ * Field handler for delete button.
  *
  * @ingroup views_field_handlers
  *
  * @ViewsField("bc_dc_delete")
  */
-class BcDcDelete extends FieldPluginBase {
+class BcDcDelete extends BcDcButtonBase {
 
   /**
    * {@inheritdoc}
    */
-  public function query() {
-    // Leave empty to avoid a query on this field.
-  }
-
-  /**
-   * Define the available options.
-   *
-   * @return array
-   *   The options.
-   */
-  protected function defineOptions() {
-    $options = parent::defineOptions();
-
-    return $options;
-  }
-
-  /**
-   * Provide the options form.
-   */
-  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
-    parent::buildOptionsForm($form, $form_state);
-  }
-
-  /**
-   * Helper function.
-   */
-  public function array_in_array($arr_needle, $arr_from) {
-    $inArray = FALSE;
-    foreach ($arr_needle as $value) {
-      if (in_array($value, $arr_from)) {
-        $inArray = TRUE;
-      }
-    }
-    return $inArray;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function render(ResultRow $values) {
-    $node = $values->_entity;
-    $user = \Drupal::currentUser();
-    /*
-    $current_lang = \Drupal::languageManager()->getCurrentLanguage()->getId();
-    $current_version_state = $this->getModerationState($node, $current_lang);
-
-    if ($current_version_state == 'archived') {
-    return '';
-    }
-     */
-
-    $options = [
-      'attributes' => [
-        'class' => ['btn', 'btn-danger'],
-        'title' => $this->t('Delete @title', ['@title' => $node->getTitle()]),
-      ],
-      'query' => [
-        'destination' => '/user/' . $user->id() . '/manage',
-      ],
+  protected function buttonConfig(): array {
+    return [
+      'text' => $this->t('Delete'),
+      'class' => 'btn-danger',
+      'route' => 'entity.node.delete_form',
     ];
-
-    $url = Url::fromRoute('entity.node.delete_form', ['node' => $node->id()], $options);
-    $text = $this->t('Delete');
-    $link = Link::fromTextAndUrl($text, $url);
-    $link_string = $link->toString();
-
-    return $link_string;
   }
 
 }
--- html/modules/custom/bc_dc/src/Plugin/views/field/BcDcUnpublish.php
+++ html/modules/custom/bc_dc/src/Plugin/views/field/BcDcUnpublish.php
@@ -2,91 +2,24 @@
 
 namespace Drupal\bc_dc\Plugin\views\field;
 
-use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Link;
-use Drupal\Core\Url;
-use Drupal\views\Plugin\views\field\FieldPluginBase;
-use Drupal\views\ResultRow;
-
 /**
- * Field handler to unpublish button .
+ * Field handler for unpublish button.
  *
  * @ingroup views_field_handlers
  *
  * @ViewsField("bc_dc_unpublish")
  */
-class BcDcUnpublish extends FieldPluginBase {
+class BcDcUnpublish extends BcDcButtonBase {
 
   /**
    * {@inheritdoc}
    */
-  public function query() {
-    // Leave empty to avoid a query on this field.
-  }
-
-  /**
-   * Define the available options.
-   *
-   * @return array
-   *   The options.
-   */
-  protected function defineOptions() {
-    $options = parent::defineOptions();
-
-    return $options;
-  }
-
-  /**
-   * Provide the options form.
-   */
-  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
-    parent::buildOptionsForm($form, $form_state);
-  }
-
-  /**
-   * Helper function.
-   */
-  public function array_in_array($arr_needle, $arr_from) {
-    $inArray = FALSE;
-    foreach ($arr_needle as $value) {
-      if (in_array($value, $arr_from)) {
-        $inArray = TRUE;
-      }
-    }
-    return $inArray;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function render(ResultRow $values) {
-    $node = $values->_entity;
-    $user = \Drupal::currentUser();
-    /*
-    $current_lang = \Drupal::languageManager()->getCurrentLanguage()->getId();
-    $current_version_state = $this->getModerationState($node, $current_lang);
-
-    if ($current_version_state == 'archived') {
-    return '';
-    }
-     */
-
-    $options = [
-      'attributes' => [
-        'class' => ['btn', 'btn-secondary'],
-        'title' => $this->t('Unpublish @title', ['@title' => $node->getTitle()]),
-      ],
-      'query' => [
-        'destination' => '/user/' . $user->id() . '/manage',
-      ],
+  protected function buttonConfig(): array {
+    return [
+      'text' => $this->t('Unpublish'),
+      'class' => 'btn-secondary',
+      'route' => 'bc_dc.data_set_archive_page',
     ];
-
-    $url = Url::fromRoute('bc_dc.data_set_archive_page', ['node' => $node->id()], $options);
-    $text = $this->t('Unpublish');
-    $link = Link::fromTextAndUrl($text, $url);
-    $link_string = $link->toString();
-
-    return $link_string;
   }
 
 }
