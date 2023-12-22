235b339ee735eb2bb973d6355047e3cbd6585d35
Liam Morland <liam@openplus.ca>
2023-12-22 16:19:42 -0500

    Issue #301: Refactor show/hide all button

--- html/themes/custom/dc_theme/js/mr.js
+++ html/themes/custom/dc_theme/js/mr.js
@@ -1,46 +1,77 @@
-// Function to update the text and aria-expanded attribute of the button
-function updateButtonText() {
-  const container = document.querySelector('.field--name-field-columns');
-  const detailsElements = container.querySelectorAll('details');
-  const button = document.getElementById('toggle-button');
+/**
+ * @file
+ * Code for button to open/close all in Data dictionary.
+ */
 
-  // Check if any details are closed
-  const anyClosed = Array.from(detailsElements).some(details => !details.open);
+/*jslint browser indent2 long this*/
 
-  // Update button text based on whether any details elements are closed
-  button.textContent = anyClosed ? 'Open all data columns' : 'Close all data columns';
+(function (document) {
 
-  // Update aria-expanded attribute
-  button.setAttribute('aria-expanded', (!anyClosed).toString());
-}
-
-// Function to toggle the open state of all details elements
-function toggleDetails() {
-  const container = document.querySelector('.field--name-field-columns');
-  const detailsElements = container.querySelectorAll('details');
-  const button = document.getElementById('toggle-button');
-
-  // Check the current action required based on button text
-  const shouldOpen = button.textContent.includes('Open all data columns');
-
-  // Set all details elements to the new state
-  detailsElements.forEach(details => {
-      details.open = shouldOpen;
-  });
-
-  // Update the button text and aria-expanded attribute
-  updateButtonText();
-}
-
-// Event listener for DOMContentLoaded
-document.addEventListener('DOMContentLoaded', function() {
-  // Show the button if JS is enabled
-  const button = document.getElementById('toggle-button');
-  if (button) {
-      button.style.display = 'block';
+  /**
+   * Return all the details elements that need to be toggled.
+   *
+   * @return NodeList
+   *   A the details elements as returned by querySelectorAll().
+   */
+  function getDetailsElements() {
+    return document.querySelectorAll(".field--name-field-columns details");
   }
 
-  // Initialize button text and attach event listener
-  updateButtonText();
-  button.addEventListener('click', toggleDetails);
-});
+  /**
+   * Update the text and aria-expanded attribute of the button.
+   *
+   * @param button
+   *   The button to update.
+   * @param detailsElements
+   *   The the details elements that are toggled.
+   */
+  function updateButtonText(button, detailsElements) {
+    // Check if any details are closed.
+    const anyOpen = Array.from(detailsElements).some((details) => details.open);
+
+    // Update button text based on whether any details elements are closed.
+    button.textContent = (
+      anyOpen
+      ? "Close all data columns"
+      : "Open all data columns"
+    );
+
+    // Update aria-expanded attribute.
+    button.setAttribute("aria-expanded", anyOpen.toString());
+  }
+
+  /**
+   * Toggle the open state of the details elements.
+   */
+  function toggleDetails() {
+    const detailsElements = getDetailsElements();
+
+    // Check the current action required based on @aria-expanded.
+    const shouldOpen = this.getAttribute("aria-expanded") === "false";
+
+    // Set all details elements to the new state.
+    detailsElements.forEach(function (details) {
+      details.open = shouldOpen;
+    });
+
+    // Update the button text and aria-expanded attribute.
+    updateButtonText(this, detailsElements);
+  }
+
+  /**
+   * Event listener for DOMContentLoaded.
+   */
+  document.addEventListener("DOMContentLoaded", function () {
+    // The button to act on.
+    const button = document.getElementById("dc-data-dictionary-toggle-button");
+    if (button) {
+      // Display the button. Without JS it is hidden by CSS.
+      button.style.display = "block";
+      // Initialize button text.
+      updateButtonText(button, getDetailsElements());
+      // Attach event listener.
+      button.addEventListener("click", toggleDetails);
+    }
+  });
+
+}(document));
