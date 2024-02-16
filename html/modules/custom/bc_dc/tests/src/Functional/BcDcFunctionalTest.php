bcgov/MFIN-Data-Catalogue

commit eb3eed9558608608052d91edce16859b8eaf9618

Issue #335: Change permissions on dashboard views so elements match role

config/sync/views.view.bookmarks.yml
@@ -7,10 +7,6 @@ dependencies:
     - field.storage.node.field_modified_date
     - flag.flag.bookmark
     - node.type.data_set
-    - user.role.administrator
-    - user.role.data_catalogue_administrator
-    - user.role.data_catalogue_editor
-    - user.role.data_catalogue_manager
   module:
     - datetime
     - flag
@@ -410,13 +406,9 @@ display:
           sort_asc_label: Asc
           sort_desc_label: Desc
       access:
-        type: role
+        type: perm
         options:
-          role:
-            administrator: administrator
-            data_catalogue_administrator: data_catalogue_administrator
-            data_catalogue_manager: data_catalogue_manager
-            data_catalogue_editor: data_catalogue_editor
+          perm: 'access content'
       cache:
         type: none
         options: {  }
@@ -552,7 +544,7 @@ display:
         - 'languages:language_interface'
         - url
         - 'user.node_grants:view'
-        - user.roles
+        - user.permissions
       tags:
         - 'config:field.storage.flagging.field_last_viewed_date'
         - 'config:field.storage.node.field_modified_date'
@@ -853,7 +845,7 @@ display:
         - 'languages:language_interface'
         - url
         - 'user.node_grants:view'
-        - user.roles
+        - user.permissions
       tags: {  }
   dashboard_bookmarks:
     id: dashboard_bookmarks
@@ -1273,7 +1265,7 @@ display:
         - 'languages:language_content'
         - 'languages:language_interface'
         - 'user.node_grants:view'
-        - user.roles
+        - user.permissions
       tags:
         - 'config:field.storage.flagging.field_last_viewed_date'
         - 'config:field.storage.node.field_modified_date'
@@ -1692,7 +1684,7 @@ display:
         - url
         - url.query_args
         - 'user.node_grants:view'
-        - user.roles
+        - user.permissions
       tags:
         - 'config:field.storage.flagging.field_last_viewed_date'
         - 'config:field.storage.node.field_modified_date'

config/sync/views.view.saved_searches.yml
@@ -2,11 +2,6 @@ uuid: 36406b2b-5575-4a3a-a740-a0dac5179175
 langcode: en
 status: true
 dependencies:
-  config:
-    - user.role.administrator
-    - user.role.data_catalogue_administrator
-    - user.role.data_catalogue_editor
-    - user.role.data_catalogue_manager
   module:
     - options
     - search_api_saved_searches
@@ -440,13 +435,9 @@ display:
           sort_asc_label: Asc
           sort_desc_label: Desc
       access:
-        type: role
+        type: perm
         options:
-          role:
-            administrator: administrator
-            data_catalogue_administrator: data_catalogue_administrator
-            data_catalogue_manager: data_catalogue_manager
-            data_catalogue_editor: data_catalogue_editor
+          perm: 'access content'
       cache:
         type: none
         options: {  }
@@ -592,7 +583,7 @@ display:
         - 'languages:language_interface'
         - url
         - url.query_args
-        - user.roles
+        - user.permissions
       tags: {  }
   dashboard_saved_search:
     id: dashboard_saved_search
@@ -1079,7 +1070,7 @@ display:
         - 'languages:language_content'
         - 'languages:language_interface'
         - url
-        - user.roles
+        - user.permissions
       tags: {  }
   page:
     id: page
@@ -1539,5 +1530,5 @@ display:
         - 'languages:language_interface'
         - url
         - url.query_args
-        - user.roles
+        - user.permissions
       tags: {  }

config/sync/views.view.dashboard_blocks.yml
@@ -319,7 +319,7 @@ display:
       access:
         type: perm
         options:
-          perm: 'access content'
+          perm: 'create data_set content'
       cache:
         type: none
         options: {  }

commit df2790810658f9c019d16b9c9533a27160f83d2d

Issue #335: Restrict bookmarks and saved searches Views to authenticated users
Fixes issue introduced in eb3eed9.

config/sync/views.view.bookmarks.yml
@@ -7,6 +7,7 @@ dependencies:
     - field.storage.node.field_modified_date
     - flag.flag.bookmark
     - node.type.data_set
+    - user.role.authenticated
   module:
     - datetime
     - flag
@@ -406,9 +407,10 @@ display:
           sort_asc_label: Asc
           sort_desc_label: Desc
       access:
-        type: perm
+        type: role
         options:
-          perm: 'access content'
+          role:
+            authenticated: authenticated
       cache:
         type: none
         options: {  }
@@ -544,7 +546,7 @@ display:
         - 'languages:language_interface'
         - url
         - 'user.node_grants:view'
-        - user.permissions
+        - user.roles
       tags:
         - 'config:field.storage.flagging.field_last_viewed_date'
         - 'config:field.storage.node.field_modified_date'
@@ -845,7 +847,7 @@ display:
         - 'languages:language_interface'
         - url
         - 'user.node_grants:view'
-        - user.permissions
+        - user.roles
       tags: {  }
   dashboard_bookmarks:
     id: dashboard_bookmarks
@@ -1280,7 +1282,7 @@ display:
         - 'languages:language_content'
         - 'languages:language_interface'
         - 'user.node_grants:view'
-        - user.permissions
+        - user.roles
       tags:
         - 'config:field.storage.flagging.field_last_viewed_date'
         - 'config:field.storage.node.field_modified_date'
@@ -1699,7 +1701,7 @@ display:
         - url
         - url.query_args
         - 'user.node_grants:view'
-        - user.permissions
+        - user.roles
       tags:
         - 'config:field.storage.flagging.field_last_viewed_date'
         - 'config:field.storage.node.field_modified_date'

config/sync/views.view.saved_searches.yml
@@ -2,6 +2,8 @@ uuid: 36406b2b-5575-4a3a-a740-a0dac5179175
 langcode: en
 status: true
 dependencies:
+  config:
+    - user.role.authenticated
   module:
     - options
     - search_api_saved_searches
@@ -435,9 +437,10 @@ display:
           sort_asc_label: Asc
           sort_desc_label: Desc
       access:
-        type: perm
+        type: role
         options:
-          perm: 'access content'
+          role:
+            authenticated: authenticated
       cache:
         type: none
         options: {  }
@@ -583,7 +586,7 @@ display:
         - 'languages:language_interface'
         - url
         - url.query_args
-        - user.permissions
+        - user.roles
       tags: {  }
   dashboard_saved_search:
     id: dashboard_saved_search
@@ -1075,7 +1078,7 @@ display:
         - 'languages:language_content'
         - 'languages:language_interface'
         - url
-        - user.permissions
+        - user.roles
       tags: {  }
   page:
     id: page
@@ -1535,5 +1538,5 @@ display:
         - 'languages:language_interface'
         - url
         - url.query_args
-        - user.permissions
+        - user.roles
       tags: {  }

html/modules/custom/bc_dc/tests/src/ExistingSite/BcDcExistingSiteTest.php
@@ -139,9 +139,20 @@ class BcDcExistingSiteTest extends BcbbExistingSiteBase {
       [div/a[starts-with(@href, "/search?f%5B0%5D=series%3A")]]');
 
     // Test Dashboard for DC user.
+    // These tests are here instead of in Functional tests because search does
+    // not work properly in Functional.
     $this->drupalGet('user');
     // View block saved_searches appears.
     $this->assertSession()->elementExists('xpath', '//div[contains(@class, "block-views-blocksaved-searches-dashboard-saved-search")]');
+    // Access to saved searches page.
+    $this->drupalGet('user/1/saved-searches');
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Anonymous.
+    $this->clickLink('Log out');
+    // No access to saved searches page.
+    $this->drupalGet('user/1/saved-searches');
+    $this->assertSession()->statusCodeEquals(404);
   }
 
 }

html/modules/custom/bc_dc/tests/src/Functional/BcDcFunctionalTest.php
@@ -1397,6 +1397,22 @@ https?://[^/]+/node/2)', htmlspecialchars_decode($gcnotify_request->rows[1][2]))
     ];
     $xpath = $this->assertSession()->buildXPathQuery('//ol/li//a[contains(text(), :data_set_title_2)]', $args);
     $this->assertSession()->elementExists('xpath', $xpath, $details);
+    // Access to bookmarks page.
+    $this->drupalGet('user/1/bookmarks');
+    $this->assertSession()->statusCodeEquals(200);
+    // Access to saved searches page.
+    // This test is in ExistingSite because search does not work in Functional.
+    // $this->drupalGet('user/1/saved-searches');
+    // $this->assertSession()->statusCodeEquals(200);
+    //
+    // Anonymous.
+    $this->drupalLogout();
+    // No access to bookmarks page.
+    $this->drupalGet('user/1/bookmarks');
+    $this->assertSession()->statusCodeEquals(404);
+    // No access to saved searches page.
+    $this->drupalGet('user/1/saved-searches');
+    $this->assertSession()->statusCodeEquals(404);
   }
 
   /**