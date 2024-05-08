bcgov/MFIN-Data-Catalogue

commit ec6c475160e9eaeed739a9ee1f86ee5147961bb1

Issue #238: Create page to unpublish data_set nodes

html/modules/custom/bc_dc/src/Form/BcDcArchiveForm.php
@@ -1,0 +1,153 @@
+<?php
+
+namespace Drupal\bc_dc\Form;
+
+use Drupal\bc_dc\Access\BuildEditAccess;
+use Drupal\content_moderation\ModerationInformation;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Access\AccessResultInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\ConfirmFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Routing\Access\AccessInterface;
+use Drupal\Core\Routing\RouteMatch;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\Url;
+use Drupal\node\NodeInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ * Implements a form to archive a node.
+ */
+class BcDcArchiveForm extends ConfirmFormBase implements AccessInterface {
+
+  /**
+   * ID of the node to archive.
+   *
+   * @var int
+   */
+  protected int $id;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\content_moderation\ModerationInformation $contentModerationModerationInformation
+   *   The content_moderation.moderation_information service.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity_type.manager service.
+   */
+  public function __construct(
+    protected ModerationInformation $contentModerationModerationInformation,
+    protected EntityTypeManagerInterface $entityTypeManager,
+  ) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container): static {
+    return new static(
+      $container->get('content_moderation.moderation_information'),
+      $container->get('entity_type.manager'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId(): string {
+    return 'node_archive_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, NodeInterface $node = NULL): array {
+    // Return 404 if this is not a node that can be moderated.
+    $node_definition = $this->entityTypeManager->getDefinition('node');
+    if (!$node || !$this->contentModerationModerationInformation->shouldModerateEntitiesOfBundle($node_definition, $node->bundle())) {
+      throw new NotFoundHttpException();
+    }
+
+    // Store the form ID for later use.
+    $this->id = (int) $node->id();
+
+    // Get the default form.
+    $form = parent::buildForm($form, $form_state);
+
+    // If the node is not published, set a message and hide the submit button.
+    if (!$node->isPublished()) {
+      $message = $this->t('This metadata record is already unpublished.');
+      $this->messenger()->addError($message);
+
+      $form['actions']['submit']['#access'] = FALSE;
+    }
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCancelUrl(): Url {
+    return Url::fromRoute('bc_dc.data_set_build_page_tab', ['node' => $this->id]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getQuestion(): TranslatableMarkup {
+    $node = $this->entityTypeManager->getStorage('node')->load($this->id);
+
+    return $this->t('Do you want to unpublish %title?', ['%title' => $node->getTitle()]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDescription(): TranslatableMarkup {
+    return $this->t('You may re-publish it later.');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    $node = $this->entityTypeManager->getStorage('node')->load($this->id);
+
+    // Archive the node.
+    $node->set('moderation_state', 'archived')->save();
+
+    // Set status message.
+    $message = $this->t('Unpublished %title.', ['%title' => $node->getTitle()]);
+    $this->messenger()->addStatus($message);
+
+    // If no destination param is provided, go to "Build" page.
+    if (!$this->getRequest()->get('destination')) {
+      $form_state->setRedirectUrl($this->getCancelUrl());
+    }
+  }
+
+  /**
+   * Custom access check for this form.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   Run access checks for this account.
+   * @param \Drupal\Core\Routing\RouteMatch $route_match
+   *   The RouteMatch object.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   The access result.
+   */
+  public static function access(AccountInterface $account, RouteMatch $route_match): AccessResultInterface {
+    // The user must have the permission plus be able to edit the node.
+    if ($account->hasPermission('archive data_set nodes')) {
+      return BuildEditAccess::access($account, $route_match);
+    }
+
+    return AccessResult::neutral();
+  }
+
+}

html/modules/custom/bc_dc/bc_dc.links.task.yml
@@ -1,3 +1,8 @@
+bc_dc.data_set_archive_page:
+  route_name: bc_dc.data_set_archive_page
+  base_route: entity.node.canonical
+  title: 'Unpublish'
+
 bc_dc.data_set_build_page_tab:
   route_name: bc_dc.data_set_build_page_tab
   base_route: entity.node.canonical

html/modules/custom/bc_dc/bc_dc.module
@@ -43,6 +43,14 @@ function bc_dc_entity_operation(EntityInterface $entity): array {
     ];
   }
 
+  $url = Url::fromRoute('bc_dc.data_set_archive_page', ['node' => $entity->id()]);
+  if ($url->access()) {
+    $operations['bc_dc_archive'] = [
+      'title' => t('Unpublish'),
+      'url' => $url,
+    ];
+  }
+
   return $operations;
 }
 

html/modules/custom/bc_dc/bc_dc.permissions.yml
@@ -1,3 +1,6 @@
+'archive data_set nodes':
+  title: 'Unpublish metadata records'
+
 'assign data_catalogue_administrator role':
   title: 'Assign role data_catalogue_administrator'
   description: 'Used by role_delegation. Declared here so that tests will run.'

html/modules/custom/bc_dc/bc_dc.routing.yml
@@ -1,3 +1,16 @@
+bc_dc.data_set_archive_page:
+  path: '/node/{node}/unpublish'
+  defaults:
+    _title: 'Unpublish'
+    _form: '\Drupal\bc_dc\Form\BcDcArchiveForm'
+  requirements:
+    _custom_access: '\Drupal\bc_dc\Form\BcDcArchiveForm::access'
+  options:
+    parameters:
+      node:
+        type: 'entity:node'
+        load_latest_revision: true
+
 bc_dc.data_set_build_page_tab:
   path: '/node/{node}/build'
   requirements:

commit 70eaf47233709bb2647b2dd73566d7b50d251d9d

Issue #465: Allow access to "Unpublish" page only when the node is eligible to be unpublished
Fixes issue introduced in ec6c475.

html/modules/custom/bc_dc/src/Form/BcDcArchiveForm.php
@@ -16,7 +16,6 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\Core\Url;
 use Drupal\node\NodeInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Implements a form to archive a node.
@@ -65,25 +64,11 @@ class BcDcArchiveForm extends ConfirmFormBase implements AccessInterface {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state, NodeInterface $node = NULL): array {
-    // Return 404 if this is not a node that can be moderated.
-    $node_definition = $this->entityTypeManager->getDefinition('node');
-    if (!$node || !$this->contentModerationModerationInformation->shouldModerateEntitiesOfBundle($node_definition, $node->bundle())) {
-      throw new NotFoundHttpException();
-    }
-
     // Store the form ID for later use.
     $this->id = (int) $node->id();
 
     // Get the default form.
     $form = parent::buildForm($form, $form_state);
-
-    // If the node is not published, set a message and hide the submit button.
-    if (!$node->isPublished()) {
-      $message = $this->t('This metadata record is already unpublished.');
-      $this->messenger()->addError($message);
-
-      $form['actions']['submit']['#access'] = FALSE;
-    }
 
     return $form;
   }
@@ -141,9 +126,15 @@ class BcDcArchiveForm extends ConfirmFormBase implements AccessInterface {
    * @return \Drupal\Core\Access\AccessResultInterface
    *   The access result.
    */
-  public static function access(AccountInterface $account, RouteMatch $route_match): AccessResultInterface {
+  public function access(AccountInterface $account, RouteMatch $route_match): AccessResultInterface {
     $entity = $route_match->getParameter('node');
     $entity_owner = $entity->getOwnerId();
+
+    // No access if the node is not published or if this is not a node that can
+    // be moderated.
+    if (!$entity->isPublished() || !$this->contentModerationModerationInformation->shouldModerateEntitiesOfBundle($this->entityTypeManager->getDefinition('node'), $entity->bundle())) {
+      return AccessResult::forbidden();
+    }
 
     // The user has access if they are able to edit the entity and either has
     // the permission or are the owner of the entity.

html/modules/custom/bc_dc/tests/src/Functional/BcDcFunctionalTest.php
@@ -1602,11 +1602,18 @@ https?://[^/]+/node/2)', htmlspecialchars_decode($gcnotify_request->rows[1][2]))
     $this->assertSession()->elementExists('xpath', $xpath);
     // Check that it is unpublished.
     $this->assertSession()->elementExists('xpath', '//div[contains(@class, "node--unpublished")]');
-    // Now "Unpublish" page has a message.
-    $this->clickLink('Unpublish');
-    $text = $this->assertSession()->elementExists('xpath', '//div[contains(@class, "alert-error")]')->getText();
-    $this->assertStringContainsString('This metadata record is already unpublished.', $text);
-    $this->assertSession()->buttonNotExists('Confirm');
+    // Now "Unpublish" tab and page are not there.
+    $this->assertSession()->linkNotExists('Unpublish');
+    $this->drupalGet('node/2/unpublish');
+    $this->assertSession()->statusCodeEquals(404);
+
+    // Re-publish node/2.
+    $this->drupalGet('node/2/build');
+    $edit = [
+      'major_edit' => '0',
+    ];
+    $this->submitForm($edit, 'Publish');
+    $this->assertSession()->pageTextContains('Metadata record published');
 
     // For DC manager, no "Unpublish" link.
     $this->drupalLogin($this->users['Test Data catalogue manager']);
@@ -1619,13 +1626,6 @@ https?://[^/]+/node/2)', htmlspecialchars_decode($gcnotify_request->rows[1][2]))
     // Now "Unpublish" link exists.
     $this->drupalGet('node/2');
     $this->assertSession()->linkExists('Unpublish');
-    // Re-publish node/2.
-    $this->drupalGet('node/2/build');
-    $edit = [
-      'major_edit' => '0',
-    ];
-    $this->submitForm($edit, 'Publish');
-    $this->assertSession()->pageTextContains('Metadata record published');
 
     // Test Dashboard for DC user.
     $this->drupalLogin($this->users['Test Data catalogue user']);