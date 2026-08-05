37fb9a06b6bc623dad4fa5e174da720adfff7649
Dan Howard <dan.howard@gov.bc.ca>
2026-08-05 13:05:55 -0700

    BCBB-74: Move Monolog service def over to Gitops repo.

--- html/sites/default/settings.local.php
+++ html/sites/default/settings.local.php
@@ -35,7 +35,6 @@
  * Enable local development services.
  */
 $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
-$settings['container_yamls'][] = 'sites/default/monolog.services.yml';
 
 /**
  * Show all error messages, with backtrace information.
