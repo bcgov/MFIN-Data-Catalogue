728d775f88cee10b525dbf0a14988bc56c6144cb
Dan Howard <dan.howard@gov.bc.ca>
2026-08-26 14:23:33 -0700

    BCBB-119: Refactor file_private_path into common settings.php

--- html/sites/default/settings.php
+++ html/sites/default/settings.php
@@ -68,6 +68,22 @@ $settings['update_free_access'] = FALSE;
 
 
 /**
+ * Private file path:
+ *
+ * A local file system path where private files will be stored. This directory
+ * must be absolute, outside of the Drupal installation directory and not
+ * accessible over the web.
+ *
+ * Note: Caches need to be cleared when this value is changed to make the
+ * private:// stream wrapper available to the system.
+ *
+ * See https://www.drupal.org/documentation/modules/file for more information
+ * about securing private files.
+ */
+$settings['file_private_path'] = DRUPAL_ROOT . "/../private";
+
+
+/**
  * Load services definition file.
  */
 $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
